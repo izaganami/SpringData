09cfb970b7d2b40904e74e524f86eb6ee80d4d33
izaganami <43637314+izaganami@users.noreply.github.com>
2019-05-10 23:47:08 +0000

    Update PersonRep.java

--- TP_SpringData/src/main/java/com/example/demo/person/service/PersonRep.java
+++ TP_SpringData/src/main/java/com/example/demo/person/service/PersonRep.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+/* create,read,update and delete interface to avoid boiler plate code usually written in jpa without the spring data layer*/
 public interface PersonRep extends CrudRepository<Person, Integer> {
 	// recherche une personne par son attribut "Name"
 	List<Person> findByfirstName(String nom);
