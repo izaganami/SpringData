c48e7686ce4d5891c35ecc64574b408a7fcbea51
Younes <younesjf@outlook.fr>
2019-09-17 14:08:35 +0200

    Update app.java

--- TP_SpringData/src/main/java/com/example/demo/app.java
+++ TP_SpringData/src/main/java/com/example/demo/app.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YassinApplication {
+public class app {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YassinApplication.class, args);
+		SpringApplication.run(app.class, args);
 	}
 
 }
